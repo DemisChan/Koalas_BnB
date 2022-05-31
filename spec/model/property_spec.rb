fb4b65a626f03f4ca5103e38c5ee9022e2e2840f
Emma Samuel <emmameaden95@gmail.com>
2022-05-31 14:09:27 +0100

    plodding through

--- spec/model/property_spec.rb
+++ spec/model/property_spec.rb
@@ -4,13 +4,11 @@ RSpec.describe Property do
   it "constructs without an ID" do
     property = Property.new(
       "Rainbow Lodge",
-      "Rueil-Malmaison, France",
       "6 Rue du Gue, Rueil-Malmaison, France",
       60,
       "Beachfront property"
     )
     expect(property.name).to eq "Rainbow Lodge"
-    expect(property.location).to eq "Rueil-Malmaison, France"
     expect(property.address).to eq "6 Rue du Gue, Rueil-Malmaison, France"
     expect(property.price).to eq 60
     expect(property.description).to eq "Beachfront property"
@@ -18,15 +16,14 @@ RSpec.describe Property do
 
   it "constructs with an ID" do
     property = Property.new(
+      3
       "Rainbow Lodge",
-      "Rueil-Malmaison, France",
       "6 Rue du Gue, Rueil-Malmaison, France",
       60,
       "Beachfront property",
-      3
     )
+    expect(property.id).to eq 3
     expect(property.name).to eq "Rainbow Lodge"
-    expect(property.location).to eq "Rueil-Malmaison, France"
     expect(property.address).to eq "6 Rue du Gue, Rueil-Malmaison, France"
     expect(property.price).to eq 60
     expect(property.description).to eq "Beachfront property"
