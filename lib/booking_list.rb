83b0c866824bd1e8c658051fb066ceb95313a5ae
Emma Samuel <emmameaden95@gmail.com>
2022-06-01 13:59:59 +0100

    Update booking_list.rb

--- lib/booking_list.rb
+++ lib/booking_list.rb
@@ -8,4 +8,16 @@ class BookingList
       row_to_object(row)
     end
   end
+
+	private 
+
+	def row_to_object(row)
+		return Booking.new(
+			row["username"], 
+			row["start_date"],
+			row["end_date"],
+			row["prop_id"],
+			row["id"]
+		)
+	end
 end
