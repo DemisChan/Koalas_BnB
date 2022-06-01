e55a8a4c2aecf30c86ec8f295e8c7fcbe3416425
Emma Samuel <emmameaden95@gmail.com>
2022-06-01 16:39:47 +0100

    emma and ana pairing

--- lib/booking_list.rb
+++ lib/booking_list.rb
@@ -15,6 +15,20 @@ class BookingList
 		return result[0]["id"]
 	end
 
+  def remove(index)
+    @database.run("DELETE FROM bookings WHERE id = $1;", [index])
+  end
+
+  def update(index, username, start_date, end_date, property_id)
+    @database.run("UPDATE bookings SET username = $2, start_date = $3, end_date = $4, property_id = $5 WHERE id = $1;",
+    [index, username, start_date, end_date, property_id])
+  end
+
+  def get(index)
+    result = @database.run("SELECT * FROM bookings WHERE id = $1;", [index])
+    return row_to_object(result[0])
+  end
+
   private 
 
 	def row_to_object(row)
